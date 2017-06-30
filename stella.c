a6fdab5e5e31d95037ba1495d8d781bb3b2b4b06
jesusble <keziahblessy33@gmail.com>
2017-06-30 11:00:39 +0530

    Create stella.c

---
+++ stella.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+void main()
+
+{
+
+char a;
+
+clrscr();
+
+printf("\n enter the letter");
+
+scanf("\n %c",&a);
+
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+
+printf("\n the letter is vowel");
+
+else
+
+printf("\n the letter is consonant");
+
+getch();
+}
