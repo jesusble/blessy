0d82ba4a4524b11ace168b1f5255368561909c76
jesusble <keziahblessy33@gmail.com>
2017-06-30 10:52:46 +0530

    Create lava.c

---
+++ lava.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n enter the no.");
+scanf("\n%d",&a);
+if(a%2==0)
+printf("\n the no. is even");
+else
+printf("\n the no. odd");
+getch();
+}
